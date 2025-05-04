55608d645f3c18bdc1d875fbf06e1a941f2ca97d
auroradream04 <alvs131313@gmail.com>
2025-05-04 18:54:28 +0800

    style: change position text color in ExperienceCard to pink-400 for better emphasis

--- src/app/components/ExperienceCard.tsx
+++ src/app/components/ExperienceCard.tsx
@@ -16,7 +16,7 @@ export default function ExperienceCard({
             <p className="text-slate-400 text-[11px] lg:text-xs !mb-1 leading-none">{duration}</p>
             <div className="col-span-3">
                 <h2 className="text-slate-100 text-sm font-bold">{company}</h2>
-                <h3 className="text-slate-200 text-sm font-medium">{position}</h3>
+                <h3 className="text-pink-400 text-sm font-medium">{position}</h3>
                 <p className="text-slate-350 text-xs mt-2 lg:text-sm lg:mt-4">{description}</p>
             </div>
         </div>
